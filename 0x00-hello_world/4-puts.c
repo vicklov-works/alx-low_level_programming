79d085e5aa8b65fb2ee5fcfbd444f2ef46b34448
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-09 12:24:19 -0700

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 	/**
-	* main - Prints "Programming is like building a multilingual puzzle
-	* 
+	* main- Prints "Programming is like building a multilingual puzzle 
 	* Return: Always 0 (Success)
 	*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }

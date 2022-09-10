33f89f87c46363562b9a51867552be1ff85ae697
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-09 20:49:26 -0700

    alx

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 10; number++)
+		putchar(number);
+
+	putchar('\n');
+
+	return (0);
+}

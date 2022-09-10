ec066b3779c165a9a0f1f441980f89bab7638a58
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-09 19:45:31 -0700

    alx

--- 0x01-variables_if_else_while/0-positve_or_negative.c
+++ 0x01-variables_if_else_while/0-positve_or_negative.c
@@ -12,12 +12,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0){
-		printf("n is positive\n" );
-	}else if(n = 0){
-		printf("n is zero\n" );
-	}else {
-		printf("n is negative\n" );
+	if (n > 0)
+	{
+		printf("n is positive\n");
+	} else if (n == 0)
+	{
+		printf("n is zero\n");
+	} else
+	{
+		printf("n is negative\n");
 	}
 	return (0);
 }

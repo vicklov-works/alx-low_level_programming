98c33a456ff7f6650e3ae5ad5d67722827750936
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-14 05:23:11 -0700

    alx

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,13 @@
 
 void print_alphabet_x10(void)
 {
+	int i;
 	char alphab;
 
+	for (i = 1; i <= 10; i++)
+	{
 	for (alphab = 'a' ; alphab <= 'z' ; alphab++)
 		putchar(alphab * 10);
-
+	}
 	putchar('\n');
 }

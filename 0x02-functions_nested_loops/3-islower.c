23bf128a0a30dc9212e232d8311785451dca3885
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-14 08:24:10 -0700

    alx

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+
+/**
+ * _islower - checks for lowercase character
+ * c - input that is to be checked
+ *
+ * Returns: void
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		putchar(1);
+	}
+	else
+	{
+		putchar(0);
+	}
+}

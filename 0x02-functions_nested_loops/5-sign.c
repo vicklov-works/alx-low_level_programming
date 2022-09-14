5de7d55a657dcd17c84645259b2d2aa3c9aee5b8
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-14 13:31:13 -0700

    alx

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 
 /**
  * print_sign - function that checks for sign
@@ -11,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf(" + ");
+		putchar(" + ");
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf(" 0 ");
+		putchar(" 0 ");
 		return (0);
 	}
 	else
 	{
-		printf(" - ");
+		putchar(" - ");
 		return (-1);
 	}
 }

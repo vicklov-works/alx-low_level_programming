0bad8535aed0982821ff95f7c4c48970dc0f48c4
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-14 10:49:49 -0700

    alx

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+
+/**
+ * print_sign - function that checks for sign
+ * @n: character to be checked
+ *
+ * Return: 1 for positive, 0 for zero and -1 for negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf(" + ");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf(" 0 ");
+		return (0);
+	}
+	else
+	{
+		printf(" - ");
+		return (-1);
+	}
+}

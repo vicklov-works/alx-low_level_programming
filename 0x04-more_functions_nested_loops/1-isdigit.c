8a07c4914c37c23152a42374e9e29d77a2636bc7
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-15 16:24:43 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+
+/**
+ * _isdigit - function that checks for digit from 0 through 9
+ * @c: character to be checked
+ *
+ * Return: 1 if digit, 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

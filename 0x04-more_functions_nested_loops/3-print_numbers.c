a085cbd148a573664a31c56651aaf7b5a99b14ce
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-16 10:45:36 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+
+/**
+ * print_numbers - function thag prints 1 to 9
+ * Return: 0 for success
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}

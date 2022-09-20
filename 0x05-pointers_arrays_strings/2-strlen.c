9da3bdb33c9caa0a4722ec2ed43e70db4d3887fc
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-20 16:07:10 -0700

    alx

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function to check for the length of a string
+ * @s: character for checking
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; *s[len] != '\0'; ++len)
+		putchar(len + '0');
+}

efe1c21653de70663f6ad66dc505df6954460c9b
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-20 15:38:37 -0700

    alx

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
-include "main.h"
+#include "main.h"
 
 /**
  * reset_to_98 - function that resets variable n to 98
  * @n: varaible to reset
  *
- * return: 0 for success
+ * return: nothing
  */
 
 void reset_to_98(int *n)

55b4936ad48c2c683619407663adb6ed3f6ad067
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-21 10:28:23 -0700

    alx

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - function that reverse a string
+ * @s: character to be reversed
+ * return: 0
+ */
+
+void rev_string(char *s)
+{
+	if (*s != '\0')
+	{
+		rev_string(s + 1);
+		_putchar(*s)
+	}
+}

bf02f1eb0cb89d2c1367ab6f92797ae9b82f36d1
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-20 17:00:16 -0700

    alx

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - function to print a string
+ * @str: string to be printed
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	char *str;
+
+	putchar(str);
+}

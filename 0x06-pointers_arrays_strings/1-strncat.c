5c8a17ecb06987f7f5442fe435bc10ec2f054184
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-22 15:00:43 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio h>
+
+/**
+ * _strncat - functions that concatenate two strings
+ * @dest: destination
+ * @src: source
+ * @n: byte of string
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int totald = 0;
+	int totals = 0;
+	int a = 0, b = 0, i = 0;
+	char allst[20000];
+
+	while (dest[totald] != '\0')
+	{
+		allst[a] = dest[totald];
+		totald++;
+		a++;
+	}
+
+	while (totals < n)
+	{
+		allst[a] = src[totals];
+		totals++;
+		a++;
+	}
+
+	while (allst[b] != '\0')
+	{
+		dest[i] = allst[b];
+		i++;
+		b++;
+	}
+
+	return (dest);
+}

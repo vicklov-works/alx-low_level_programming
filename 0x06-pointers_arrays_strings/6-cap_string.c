f10443b436dd54f4a4caca8f64408a9843521593
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-22 15:41:48 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* cap_string - function that capitalizes all words of a string
+* @c: string to be capitalized
+*
+* Return: c
+*/
+
+char *cap_string(char *c)
+{
+	int i;
+
+	if (c[0] >= 97 && c[0] <= 122)
+		c[0] -= 32;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] == 32 || c[i]  == 9 || c[i] == 10 || c[i] == 44 || c[i] == 59)
+		{
+			if (c[i + 1] >= 97 && c[i + 1] <= 122)
+			{
+				c[i + 1] -= 32;
+			}
+		}
+
+
+		if (c[i] == 46 || c[i] == 33 || c[i] == 63 || c[i] == 34 || c[i] == 40)
+		{
+			if (c[i + 1] >= 97 && c[i + 1] <= 122)
+			{
+				c[i + 1] -= 32;
+			}
+		}
+
+
+		if (c[i] == 41 || c[i] == 123 || c[i] == 124)
+		{
+			if (c[i + 1] >= 97 && c[i + 1] <= 122)
+			{
+				c[i + 1] -= 32;
+			}
+		}
+	}
+
+	return (c);

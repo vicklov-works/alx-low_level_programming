6b4f24677a48c6f5ef628daaab24b401567fb840
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-22 16:13:44 -0700

    alx

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,16 +9,18 @@
 
 void rev_string(char *s)
 {
-
+	char rev = s[0];
 	int c = 0;
+	int i;
 
-	while (*(s + c))
+	while (s[c] != '\0')
 		c++;
-	c = c - 1;
-	while (c >= 0)
+
+	for (i = 0; i < count; i++)
 	{
-		_putchar(*(s + c));
 		c--;
+		rev = s[i];
+		s[i] = s[c];
+		s[c] = rev;
 	}
-	_putchar('\n');
 }

3467111637d6a2c7f699ced827e70a422c3838a8
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-22 15:22:36 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strcmp - Compares two strings
+* @s1: String one
+* @s2: String two
+*
+* Return: value after subtraction
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int sub = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+		{
+			continue;
+		}
+		else
+		{
+			if (s2[i] != '\0')
+			{
+				diff = s1[i] - s2[i];
+				break;
+			}
+		}
+	}
+
+	return (sub);
+}

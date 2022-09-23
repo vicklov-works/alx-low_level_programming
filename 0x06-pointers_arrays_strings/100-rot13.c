289113217754019c3c0445e560de11e79bf04390
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-23 16:09:00 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* rot13 - function that encodes a string using rot13
+* @s: String
+*
+* Return: encoded string
+*/
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	int rotA[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		'W', 'X', 'Y', 'Z'};
+	int rotB[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		'J', 'K', 'L', 'M'};
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		i = 0;
+		while (rotA[i] != '\0' && s[j] != rotA[i])
+			i++;
+
+		if (s[j] == rotA[i])
+			s[j] = rotB[i];
+	}
+
+	return (s);
+
+}

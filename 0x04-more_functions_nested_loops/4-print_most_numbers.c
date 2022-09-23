5616b212e7269bcca19d283eef8671afd765597c
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-22 21:50:15 -0700

    alx

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,7 +13,7 @@ void print_most_numbers(void)
 	for (num = 0; num <= 9; num++)
 	{
 		if (num != 2 && num != 4)
-			putchar(num + '0');
+			_putchar(num + '0');
 	}
-	putchar('\n');
+	_putchar('\n');
 }

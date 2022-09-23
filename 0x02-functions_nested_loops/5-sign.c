689679dfcf9d5a1e94b05525f8d3ecc902438d75
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-23 07:21:35 -0700

    alx

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,26 @@
+#include "main.h"
 
 /**
- * print_sign - function that checks for sign
- * @n: character to be checked
- *
- * Return: 1 for positive, 0 for zero and -1 for negative
- */
-
+* print_sign - Function that prints the sign of a number
+* @n: Number to be checked
+*
+* Return: 1 if n > 0, 0 if n = 0 and -1 if n < 0
+*/
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(" + ");
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar(" 0 ");
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar(" - ");
+		_putchar('-');
 		return (-1);
 	}
 }

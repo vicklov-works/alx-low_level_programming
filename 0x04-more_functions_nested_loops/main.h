60c1ea7fe5e56fee11f8dc4d1b75f1113e3d28b8
Alabi Ifeoluwa <alabiifeoluwa0002@gmail.com>
2022-09-22 21:45:48 -0700

    alx

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _putchar(void);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -6,3 +10,5 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+
+#endif /* MAIN_H */
